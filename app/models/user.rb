ff1619b0471c56a87f17cb289c0228c076f7c64a
zfrahmd <zfr.ahmd5661@gmail.com>
2025-04-30 03:12:04 +0500

    Add devise views & user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   
-  enum role: [:user, :manager, :admin]
+  enum :role, [:user, :manager, :admin]
 
   validates :first_name, presence: true
   after_initialize :set_default_user_type, if: :new_record?
