c307ddbe07274338dceeb5a47038a336c0f6b6b3
zfrahmd <zfr.ahmd5661@gmail.com>
2025-04-30 03:51:54 +0500

    set new user as user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   enum :role, [:user, :manager, :admin]
 
   validates :first_name, presence: true
-  after_initialize :set_default_user_type, if: :new_record?
+  after_initialize :set_default_user_role, if: :new_record?
 
   # set the default usertype
-  def set_default_user_type
-    self.user_type ||= :user
+  def set_default_user_role
+    self.role ||= :user
   end
 end
