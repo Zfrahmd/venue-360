0ed248f2e1a5ee48d93060ca959cd6b8be363196
zfrahmd <zfr.ahmd5661@gmail.com>
2025-05-05 06:31:57 +0500

    Added Gallary to landing page, image viewer, phone number to inquiry, css changes

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,6 +3,10 @@ class Inquiry < ApplicationRecord
 
   validates :first_name, :email, :words, presence: true
 
+  def phone_exists
+    self.phone.nil? ? "not available" : self.phone
+  end
+
   def user_name
     return self.first_name + " " + self.last_name
   end
