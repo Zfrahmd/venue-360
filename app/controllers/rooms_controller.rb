01f60019151a1d93fb75c199c5d7171c23244391
zfrahmd <zfr.ahmd5661@gmail.com>
2025-05-16 16:41:00 +0000

    * Added rooms links in sidebar for admin * Added form to create room * Changes to navigation links

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,9 @@ class RoomsController < ApplicationController
   end
 
   def create
+    # byebug
     @room = @room.assign_attributes(room_params)
+    @room.room_images.build
     if @room.save
         redirect_back fallback_location: root_path, notice: "The room was created successfully"
     else
@@ -18,6 +20,7 @@ class RoomsController < ApplicationController
 
   def room_params
     params.require(:room).permit(:name, :headline, :description, :room_number, 
-                                 :room_type, :price_per_night, :capacity, :available)
+                                 :room_type, :price_per_night, :capacity, :available,
+                                 room_images_attributes: [:image_url])
   end
 end
